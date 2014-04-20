498f431de8798d40b18709309c418f067c6ddf02
Jed Mao <jed.hunsaker@gmail.com>
2014-04-20 16:03:52 -0500

    Add docs and specs for string helpers

--- lib/helpers/string.ts
+++ lib/helpers/string.ts
@@ -4,6 +4,9 @@ var STRING_DASHERIZE_CACHE = {};
 var STRING_DECAMELIZE_REGEXP = (/([a-z\d])([A-Z])/g);
 // ReSharper restore InconsistentNaming
 
+/**
+	Replaces underscores, spaces, or camelCase with dashes.
+*/
 export function dasherize(str) {
 	var cache = STRING_DASHERIZE_CACHE;
 	var hit = cache.hasOwnProperty(str);
@@ -15,6 +18,9 @@ export function dasherize(str) {
 	return cache[str] = decamelize(str).replace(STRING_DASHERIZE_REGEXP, '-');
 }
 
+/**
+	Converts a camelized string into all lower case separated by underscores.
+*/
 export function decamelize(str) {
 	return str.replace(STRING_DECAMELIZE_REGEXP, '$1_$2').toLowerCase();
 }
---
+++ test/spec/lib/helpers/string.spec.ts
@@ -0,0 +1,25 @@
+﻿import sinonChai = require('../../../sinon-chai');
+var expect = sinonChai.expect;
+import Blink = require('../../../../lib/Blink');
+
+
+var s = require('../../../../lib/helpers/string');
+
+// ReSharper disable WrongExpressionStatement
+describe('string helper', () => {
+
+	it('dasherizes strings', () => {
+		expect(s.dasherize('fooBarBaz')).to.eq('foo-bar-baz');
+		expect(s.dasherize('foo_bar_baz')).to.eq('foo-bar-baz');
+		expect(s.dasherize('foo-bar-baz')).to.eq('foo-bar-baz');
+		expect(s.dasherize('foo bar baz')).to.eq('foo-bar-baz');
+	});
+
+	it('decamelizes strings', () => {
+		expect(s.decamelize('fooBarBaz')).to.eq('foo_bar_baz');
+		expect(s.decamelize('foo_bar_baz')).to.eq('foo_bar_baz');
+		expect(s.decamelize('foo-bar-baz')).to.eq('foo-bar-baz');
+		expect(s.decamelize('foo bar baz')).to.eq('foo bar baz');
+	});
+
+});
