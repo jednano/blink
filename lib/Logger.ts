b16eec9263286bcb21199a77a6dc864dcbf45e8a
Jed Mao <jed.hunsaker@gmail.com>
2014-04-23 00:34:16 -0500

    Introduce Logger

---
+++ lib/Logger.ts
@@ -0,0 +1,25 @@
+﻿///<reference path="../bower_components/dt-node/node.d.ts" />
+import events = require('events');
+
+
+class Logger extends events.EventEmitter {
+
+	public debug(message: string) {
+		this.emit('debug', message);
+	}
+
+	public info(message: string) {
+		this.emit('info', message);
+	}
+
+	public warn(message: string) {
+		this.emit('warn', message);
+	}
+
+	public error(err: Error) {
+		this.emit('error', err);
+	}
+
+}
+
+export = Logger;
