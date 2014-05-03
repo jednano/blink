61c17f71bd34d789bd8a3e0f53b0f1dab5206990
Jed Mao <jed.hunsaker@gmail.com>
2014-05-03 11:14:55 -0500

    Fix ignores

---
+++ test/spec/lib/Compiler.spec.ts
@@ -0,0 +1,43 @@
+﻿import sinonChai = require('../../sinon-chai');
+var expect = sinonChai.expect;
+import Blink = require('../../../lib/Blink');
+
+
+var config = Blink.config;
+var newline = config.newline;
+
+// ReSharper disable WrongExpressionStatement
+describe('Compiler', () => {
+
+	var compiler: Blink.Compiler;
+	beforeEach(() => {
+		compiler = new Blink.Compiler(config);
+	});
+
+	it('compiles extenders', () => {
+		// ReSharper disable once JsFunctionCanBeConvertedToLambda
+		function extender() {
+			return () => {
+				return [
+					['foo', 'bar'],
+					['baz', 'qux']
+				];
+			};
+		};
+		var rules = [
+			new Blink.Rule(['.foo'], {
+				extend: [ extender ]
+			}),
+			new Blink.Rule(['.bar'], {
+				extend: [ extender ]
+			})
+		];
+		expect(compiler.compileRules(rules)).to.eq([
+			'.foo, .bar {',
+			'  foo: bar;',
+			'  baz: qux;',
+			'}'
+		].join(newline) + newline);
+	});
+
+});
