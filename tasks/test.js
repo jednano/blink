ccf71cc841c807f291e58318b4304125a1db5024
Jed Mao <jed.hunsaker@gmail.com>
2014-09-26 22:00:57 -0500

    Add code coverage

--- tasks/test.js
+++ tasks/test.js
@@ -1,17 +1,22 @@
 ﻿var gulp = require('gulp');
+var istanbul = require('gulp-istanbul');
 var mocha = require('gulp-mocha');
 
+var gutil = require('gulp-util');
+
 function test() {
-	return gulp.src([
-			'js/test/*.js',
-			'js/test/spec/**/*.js'
-		], {
-			read: false
-		})
-		.pipe(mocha({
-			reporter: 'spec',
-			clearRequireCache: true
-		}));
+	return gulp.src('js/lib/**/*.js')
+		.pipe(istanbul())
+		.on('finish', function() {
+			gulp.src(['js/test/*.js', 'js/test/spec/**/*.js'])
+				.pipe(mocha({
+					reporter: 'spec',
+					clearRequireCache: true
+				}))
+				.pipe(istanbul.writeReports({
+					reporters: ['lcov']
+				}));
+		});
 }
 
 module.exports = test;
